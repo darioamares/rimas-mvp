23d269d059e380ed9d2c393e673e9b5a540dcd74
darioamares <amaresdario@gmail.com>
2026-02-05 02:30:55 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
+  reactStrictMode: false,
+  eslint: {
+    // Importante: Esto permite que el build se complete aunque haya advertencias de código
+    // Es esencial para MVPs rápidos donde el código no está 100% limpio de variables sin usar.
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
