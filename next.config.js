6d8dcdb911dde941cb1628a54d15ea1328a6155e
darioamares <amaresdario@gmail.com>
2026-02-05 02:38:04 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,9 +6,16 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // Ignora errores de tipo (incluso en archivos JS) durante el despliegue
+    // Ignora errores de tipo durante el despliegue
     ignoreBuildErrors: true,
   },
+  // Desactivamos la optimización de imágenes para que el build no se cuelgue
+  images: {
+    unoptimized: true,
+  },
+  // Desactivamos el minificador SWC si el build se queda "congelado"
+  // Esto hace que el proceso sea más simple y directo
+  swcMinify: false,
 }
 
 module.exports = nextConfig
