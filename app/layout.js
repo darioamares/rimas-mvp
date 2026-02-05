06dfe8c992af3caafd74e35877b778f0d9bc6924
darioamares <amaresdario@gmail.com>
2026-02-05 13:26:43 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import './globals.css'
+import { AuthContextProvider } from '../context/AuthContext';
 
 export const metadata = {
   title: 'Rimas MVP - Master Station',
@@ -8,7 +9,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body>{children}</body>
+      <body>
+        <AuthContextProvider>
+          {children}
+        </AuthContextProvider>
+      </body>
     </html>
   )
 }
